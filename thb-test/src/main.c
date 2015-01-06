7fdce73ee05a61550c90f7b3da504bbcc74385ca
asbr90 <siebertartjom@googlemail.com>
2015-01-06 13:45:11 +0100

    To determine if binary is height-balanced

--- thb-test/src/main.c
+++ thb-test/src/main.c
@@ -1,6 +1,42 @@
 /*main: testing thb */
+#include "thb_test.h"
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+
+struct node* newNode(int data)
+{
+    struct node* node = (struct node*)
+                                malloc(sizeof(struct node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+ 
+    return(node);
+}
+
 
 int main(){
+	int height = 0;
 
-	return 0;
+	//construct test tree
+
+  struct node *root = newNode(1);  
+  root->left = newNode(2);
+  root->right = newNode(3);
+  root->left->left = newNode(4);
+  root->left->right = newNode(5);
+  root->right->left = newNode(6);
+  root->left->left->left = newNode(7);
+  root->left->left->left->left = newNode(7);
+
+  
+  if(isBalanced(root, &height))
+    printf("Tree is balanced");
+  else
+    printf("Tree is not balanced");    
+ 
+   getchar();
+	
+   return 0;
 }
