40f0e8b9c36cebf5ad2142aded134524e5f29d90
Sören Gutzeit <soeren.gutzeit@mni.thm.de>
2015-01-09 00:50:49 +0100

    see last commit

--- thb/src/thb.h
+++ thb/src/thb.h
@@ -2,13 +2,7 @@
 #define	THB_H
 
 #include "codetree.h"
-
-typedef struct name_queue {
-    struct name_queue *next;
-    struct name_queue *before;
-    char *name;
-    int precedence;
-} NameQueue;
+#include "namequeue.h"
 
 
 typedef struct uses {
@@ -17,14 +11,9 @@ typedef struct uses {
 } Uses;
 
 
-NameQueue * new_queue();
-void enqueue(NameQueue *queue, char *name, int precedence);
-NameQueue * dequeue(NameQueue *queue);
 
-NameQueue * roots(ListItem *forest);
 Uses * uses(ListItem * forest, char * name);
 
 
-void quickSort(NameQueue * head);
 
 #endif	/* THB_H */
