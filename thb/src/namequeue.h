aa85907957d73e635fbf1c0ebd70946bf0851772
Sören Gutzeit <soeren.gutzeit@mni.thm.de>
2015-01-09 00:49:51 +0100

    moved namequeue stuff to own c and h file

---
+++ thb/src/namequeue.h
@@ -0,0 +1,16 @@
+#include "codetree.h"
+
+typedef struct name_queue {
+    struct name_queue *next;
+    struct name_queue *before;
+    char *name;
+    int precedence;
+} NameQueue;
+
+
+NameQueue * new_queue();
+void enqueue(NameQueue *queue, char *name, int precedence);
+NameQueue * dequeue(NameQueue *queue);
+
+NameQueue * roots(ListItem *forest);
+void quickSort(NameQueue * head);
