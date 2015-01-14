b06204f91e46e58fba5350f3e94d5460dc5f7543
Patrick Koteras <patrick.koteras@mni.thm.de>
2015-01-14 20:52:53 +0100

    Added functions for UEVar.

--- thb/src/thb.h
+++ thb/src/thb.h
@@ -11,9 +11,19 @@ typedef struct uses {
 ListItem *forest;
 int varTempCounter;
 
+typedef struct UEVar_queue {
+    struct UEVar_queue *next;
+    Node *node;
+} UEVarQueue;
+
+UEVarQueue *UEVar;
+
 Uses * uses(ListItem * forest, char * name);
 void balance(Node *root);
 int flatten(Node *var, NameQueue *q);
+void setUEVAR(UEVarQueue *queue);
+bool cmpNode(Node *node1, Node *node2);
+bool cmpOp(Operation *op1, Operation *op2);
 int inUEVar(Node *var);
 void rebuild(NameQueue *q, Operation *op);
 Node *fold(Operation *op, Node *left, Node *right);
